d80c90ae4e31c9ba699fdfa9d42e724b65398c5a
荣波 <ol@DESKTOP-3RIRFUN>
2019-07-27 10:59:51 +0800

    grpc

--- GRpc.Client/Program.cs
+++ GRpc.Client/Program.cs
@@ -11,19 +11,17 @@ namespace GRpc.Client
     {
         static async Task Main(string[] args)
         {
-            AppContext.SetSwitch(
-                "System.Net.Http.SocketsHttpHandler.Http2UnencryptedSupport",
-                true);
-            var httpClient = new HttpClient();
+            // 设置当前上下文为http2.0请求
+            AppContext.SetSwitch("System.Net.Http.SocketsHttpHandler.Http2UnencryptedSupport", true);
+            var httpClient = new HttpClient { BaseAddress = new Uri("http://localhost:50051")};
             // The port number(50051) must match the port of the gRPC server.
-            httpClient.BaseAddress = new Uri("http://localhost:50051");
             var client = GrpcClient.Create<Greeter.GreeterClient>(httpClient);
 
             var st = new Stopwatch();
             while (Console.ReadKey().Key != ConsoleKey.Q)
             {
                 st.Restart();
-                for (int i = 0; i < 10000; i++)
+                for (int i = 0; i < 1; i++)
                 {
                     var reply = await client.SayHelloAsync(
                         new HelloRequest { Name = "GreeterClient", UpTime = new Google.Protobuf.WellKnownTypes.Timestamp { Seconds = 5851444 } });
